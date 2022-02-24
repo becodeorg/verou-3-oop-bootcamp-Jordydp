a83ddac4fd9b7e98a8faf0aaa40cf9e7ce0ed4b3
jordydp <jdepril@outlook.com>
2022-02-24 11:05:46 +0100

    Case 1 complete

--- Use Case #1/Case1.php
+++ Use Case #1/Case1.php
@@ -1,15 +1,43 @@
 <?php
 
+declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $basket =
 [
-    ['name'=> 'Banana \'s', 'pieces'=> 6, 'cost'=>1],
-    ['name'=> 'Apples', 'pieces'=> 3, 'cost'=>1.5],
-    ['name'=> 'Bottles of wine', 'pieces'=> 2, 'cost'=>10],
+    ['name'=> 'Banana \'s', 'pieces'=> 6, 'cost'=>1, 'tax' => 0.06],
+    ['name'=> 'Apples', 'pieces'=> 3, 'cost'=>1.5, 'tax'=> 0.06],
+    ['name'=> 'Bottles of wine', 'pieces'=> 2, 'cost'=>10, 'tax'=>0.21],
 
 ];
+function pre_r($array)
+{
+    echo '<pre>';
+    var_dump($array);
+    echo '</pre>';
+};
+pre_r($basket);
 
-print_r($basket);
-
-function calculatePrice(){
+function calculatePrice($array){
+    $totalPriceOfBasket = 0;
+    foreach($array as $product =>$value){
     
+    $totalPriceOfBasket += ($value['pieces'] * $value['cost']);    
+    }  
+    return "Total price of this basket = {$totalPriceOfBasket}€."; 
+
+    //pre_r($totalPriceOfBasket);
 }
+
+function calcTax($array){
+    $taxIncluded = 0;
+    foreach($array as $item =>$value){
+        $taxIncluded += (($value['pieces'] * $value['cost'])*$value['tax']);
+        
+    }
+    return "<br> You have {$taxIncluded}$ donated to the government.";
+}
+echo calculatePrice($basket);
+echo calcTax($basket);

b94d68b7676c06357149020cfa7ab35e365a0491
jordydp <jdepril@outlook.com>
2022-02-24 09:46:52 +0100

    Creating a array of products

---
+++ Use Case #1/Case1.php
@@ -0,0 +1,15 @@
+<?php
+
+$basket =
+[
+    ['name'=> 'Banana \'s', 'pieces'=> 6, 'cost'=>1],
+    ['name'=> 'Apples', 'pieces'=> 3, 'cost'=>1.5],
+    ['name'=> 'Bottles of wine', 'pieces'=> 2, 'cost'=>10],
+
+];
+
+print_r($basket);
+
+function calculatePrice(){
+    
+}
